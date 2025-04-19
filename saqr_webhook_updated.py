ccce9991114e706734498a89736d55d3658e652a
Saqer35 <alyamee88@gmail.com>
2025-04-19 03:21:56 +0300

    Add files via upload

---
+++ saqr_webhook_updated.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/webhook", methods=["POST"])
+def reply_whatsapp():
+    incoming_msg = request.values.get("Body", "").lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    reply_text = "هلا! معك صقر، السكرتير التنفيذي لعبدالمحسن الحرفش. شرايك نبدأ؟"
+    msg.body(reply_text)
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
